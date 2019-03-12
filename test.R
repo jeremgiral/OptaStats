2b66a79f837ec739991bf483503d5f0e5bc581c1
Jérémy Giral <jeremy.giral@bial-r.com>
2019-03-12 14:21:53 +0100

    add sql db

--- test.R
+++ test.R
@@ -15,13 +15,14 @@ con <- dbConnect(drv, dbname = "postgres",
 
 # Attention R écrit beaucoup en mémoire, ça risque de faire planté le pc de faire ce select sans filtre (where) ! 
 # Ca te permet d'avoir le modèle de données !
-events <- dbGetQuery(con, "select dq.nom qualifier,dq.description,dq.valeur,q.valeur,e.event_id,de.nom evenement,de.description,e.type_id,e.period_id,e.minuts,e.sec,e.idteam,e.outcome,e.x,e.y,e.t_timestamp,home_team_name,away_team_name,competition_name,game_date, period_1_start, period_2_start, eq.nom equipe_joueur,j1.nom joueur,j1.poste,j1.real_position,j1.real_position_side
+# J'ai mis les descriptions et les noms des différents événement et qualifier pour que ce soit compréhensible, mais pour faire tourner nos modèles on utilisera les ID (performance + correspond au fichier sample)
+events <- dbGetQuery(con, "select dq.nom qualifier,dq.description,dq.valeur,q.valeur,e.type_id,de.nom evenement,de.description,e.type_id,e.period_id,e.minuts,e.sec,e.idteam,e.outcome,e.x,e.y,e.t_timestamp,home_team_name,away_team_name,competition_name,game_date, period_1_start, period_2_start, eq.nom equipe_joueur,j1.nom joueur,j1.poste,j1.real_position,j1.real_position_side
 from qualifier q
 inner join events e on q.idevent=e.idevent
 inner join matchs m on e.idmatch=m.idmatch
 inner join equipe eq on eq.idequipe=e.idteam
 inner join dimqualifier dq on q.qualifier_id=dq.idqualifier
 left join joueur j1 on j1.idjoueur=e.idplayer
-left join dimevent de on de.eventid=e.event_id
+left join dimevent de on de.eventid=e.type_id
 limit 1000")
 events
